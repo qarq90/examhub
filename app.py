6c144e0b7bf6e937b802ab93785fa197fb41c35d
qarq90 <scythethroughtheheart05@gmail.com>
2025-03-04 01:34:41 +0530

    Profile 1.0 - 04/03/2025

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
 from pymongo import MongoClient
+from datetime import datetime
 import re
 
 EMAIL_REGEX = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
@@ -23,8 +24,10 @@ def sign_up():
         password = request.form.get("password")
         roll_no = request.form.get("roll_no")
         year = request.form.get("year")
+        dob = request.form.get("dob")  
+        branch = request.form.get("branch")  
 
-        if not email or not password or not roll_no or not year:
+        if not email or not password or not roll_no or not year or not dob or not branch:
             flash("All fields are required!", "error")
             return redirect(url_for("sign_up"))
 
@@ -36,13 +39,22 @@ def sign_up():
             flash("Email already exists!", "error")
             return redirect(url_for("sign_up"))
 
+        try:
+            dob_date = datetime.strptime(dob, "%Y-%m-%d")  
+        except ValueError:
+            flash("Invalid date format for DOB! Use YYYY-MM-DD.", "error")
+            return redirect(url_for("/sign_up"))
+
         user_data = {
             "name": name,
             "email": email,
             "password": password,  
             "roll_no": roll_no,
             "year": year,
+            "dob": dob,  
+            "branch": branch,  
         }
+
         users.insert_one(user_data)
 
         return redirect(url_for("log_in"))
@@ -52,14 +64,14 @@ def sign_up():
 @app.route('/auth/log-in', methods=["GET", "POST"])
 def log_in():
     if request.method == "POST":
-        name = request.form.get("name")
+        password = request.form.get("password")
         roll_no = request.form.get("roll_no")
 
-        if not name or not roll_no:
+        if not password or not roll_no:
             flash("All fields are required!", "error")
             return redirect(url_for("log_in"))
         
-        user = users.find_one({"name": name, "roll_no": roll_no})
+        user = users.find_one({"password": password, "roll_no": roll_no})
         if user:
             return redirect(url_for("home"))
         else:
@@ -67,6 +79,10 @@ def log_in():
             return redirect(url_for("log_in"))
 
     return render_template('/auth/log-in.html', hide_nav_footer=True)
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',hide_nav_footer=False)
 
 # Define a route for an API endpoint
 @app.route('/api/data')
